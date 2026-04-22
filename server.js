601d4909f350b61803dea2016ef47ca8b86817d9
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-04-22 18:30:42 +0530

    connecting mongodb in the evn

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
